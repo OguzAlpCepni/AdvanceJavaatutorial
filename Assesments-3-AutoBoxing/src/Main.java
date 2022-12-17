9166e53f5b79b26ac2a16c52aa01469250badc8a
oğuz alp çepni <oguzalpcepni@gmail.com>
2022-12-17 13:56:38 +0300

    Thirth commit

--- Assesments-3-AutoBoxing/src/Main.java
+++ Assesments-3-AutoBoxing/src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i = 10;
+
+
+
+
+        // auto-Unbox
+
+        Integer obj = Integer.valueOf(i);
+
+        System.out.println("value of Integer  obj : "+ obj);
+
+
+
+
+        // autobox
+
+        int i1=obj;
+
+        System.out.println("value of i1 : "+i1);
+
+
+
+
+        //Autobox
+
+        Character charObj= 'a';
+
+        //Auto-Unbox
+
+        char ch= charObj;
+
+        System.out.println("value of ch : "+ch);
+
+        System.out.println("value of CharObj : "+charObj);
+
     }
+
 }
