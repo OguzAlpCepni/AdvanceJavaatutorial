6c87b3dc1dff4c1126364292c2193d073afee160
oğuz alp çepni <oguzalpcepni@gmail.com>
2022-12-18 17:19:16 +0300

    sixth commit

--- Assesments-6-StaticKeyWord/src/Main.java
+++ Assesments-6-StaticKeyWord/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" start  ");
+        User user1= new User("oguz alp");
+        User user2 = new User("tayyip");
+        User user3 = new User("Kilicdaroglu");
 
+        System.out.println("counter last value :"+User.counter);
+        System.out.println("end");
 
     }
 }
--- Assesments-6-StaticKeyWord/src/User.java
+++ Assesments-6-StaticKeyWord/src/User.java
@@ -1,2 +1,18 @@
 public class User {
+    public String name;
+    public static int counter = 0;
+    static {
+        System.out.println("second Staic code line");
+        int random = (int)(Math.random()*10);
+        System.out.println("thirth random number ");
+        counter = random;
+        System.out.println("fourth random value " + random);
+
+    }
+
+    public User(String name){
+        this.name = name;
+        counter++;
+        System.out.println("counter new value :"+counter);
+    }
 }
