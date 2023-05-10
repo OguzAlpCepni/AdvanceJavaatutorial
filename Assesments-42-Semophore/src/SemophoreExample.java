72345abd4f1698c59a1cb4735e914ed3d3b2b357
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-05-10 22:02:15 +0300

    Semaphore

---
+++ Assesments-42-Semophore/src/SemophoreExample.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.Semaphore;
+
+public class Semophore {
+    private Semaphore = new
+
+    Semaphore() {
+        
+    }
+
+    public void ThreadStart(int id){
+
+    }
+}
